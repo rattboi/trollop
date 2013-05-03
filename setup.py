078f44530b14ca1d9f1a045613483940fd27e6e0
Brent Tubbs <brent.tubbs@gmail.com>
2013-05-03 15:20:57 -0700

    ver 0.0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trollop',
-    version='0.0.10',
+    version='0.0.11',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
