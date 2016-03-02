5f53ee45f268cd8d1482f46d260e5571386bf8ea
Brent Tubbs <brent.tubbs@gmail.com>
2016-03-01 19:54:33 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trollop',
-    version='0.0.15',
+    version='0.0.16',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
